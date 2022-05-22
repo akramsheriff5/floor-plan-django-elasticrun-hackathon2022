7655813c3322e91f57ce3e4e82ae0ee7ab4101a3
sidboxy <siddartha.dommala@gmail.com>
2022-05-22 20:47:49 +0530

    Analytics changes

--- floor/Analytics.py
+++ floor/Analytics.py
@@ -8,7 +8,10 @@ def HeatMap(invoice_file = None, SKU_Storage_file = None, Floor_Map_Path = None)
    SKU_Storage_file = open(SKU_Storage_file)
    SKU_storage_data = json.load(SKU_Storage_file)
    image = cv2.imread(Floor_Map_Path)
-   image = cv2.resize(image, (SKU_storage_data['canvasDimensions']['width'], SKU_storage_data['canvasDimensions']['height']))
+   originalH, originalW, _ = image.shape
+   canvasH, canvasW =  SKU_storage_data['canvasDimensions']['height'], SKU_storage_data['canvasDimensions']['width']
+   ratioH, ratioW  = (originalH / canvasH) , (originalW / canvasW)
+   # image = cv2.resize(image, (SKU_storage_data['canvasDimensions']['width'], SKU_storage_data['canvasDimensions']['height']))
    # # Saving and sending heatmap data in JSON format
    # if invoice_data:
    #  Output_JSON = invoice_data
@@ -17,8 +20,9 @@ def HeatMap(invoice_file = None, SKU_Storage_file = None, Floor_Map_Path = None)
       if IV['areaNumber'] == SKU['areaNumber']:
          HM_Ratio = int(IV['quantityR']) / int(SKU['quantity'])
          x1, y1, w, h = int(SKU['x0']), int(SKU['y0']), int(SKU['x1']), int(SKU['y1'])
-         cv2.rectangle(image, (x1, y1), (x1 + w, y1 + h), (0,255, 255), 1)
-         x2, y2 =(x1 + w, y1 + h)
+         x1, y1, x2, y2 = int(x1 * ratioW), int(y1 * ratioH), int((x1 + w) * ratioW), int((y1 + h) * ratioH)
+         cv2.rectangle(image, (x1, y1), (x2, y2), (0,255, 255), 2)
+
          # Getting Mid-Point of a rectangle
          CX, CY = int((x1 + x2) / 2), int((y1 + y2) / 2)
          # Getting Perimeter of Rectangle
@@ -26,13 +30,18 @@ def HeatMap(invoice_file = None, SKU_Storage_file = None, Floor_Map_Path = None)
          print()
          if HM_Ratio:
             cv2.circle(image, (CX, CY), int(P * HM_Ratio), (255, 0, 255), -1)
-            cv2.putText(image, "Category: %s, Quantity Selected: %s (percentage)" % (IV['areaNumber'], str(HM_Ratio * 100)),
-                     (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (255, 255, 0), thickness=1)
+            cv2.putText(image, "Category: %s " % IV['areaNumber'],
+                     (x2, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), thickness=1)
+            cv2.putText(image,
+                        "Quantity Selected: %s (percentage)" % str(HM_Ratio * 100),
+                        (x2, y1 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), thickness=1)
          else:
             cv2.circle(image, (CX, CY), int(P * HM_Ratio), (255, 0, 255), -1)
+            cv2.putText(image, "Category: %s " % IV['areaNumber'],
+                        (x2, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), thickness=1)
             cv2.putText(image,
-                     "Category: %s, Quantity Selected: %s (percentage)" % (IV['areaNumber'], str(HM_Ratio * 100)),
-                     (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0, 255, 255), thickness=1)
+                        "Quantity Selected: %s (percentage)" % str(HM_Ratio * 100),
+                        (x2, y1 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), thickness=1)
       else:
          continue
 
